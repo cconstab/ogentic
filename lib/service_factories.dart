2b53e70bc60c0a6328b1dcd461e19e046d4b6956
Colin Constable <colin@constable.cc>
2025-03-25 09:10:00 -0700

    feat: updated the name space

--- lib/service_factories.dart
+++ lib/service_factories.dart
@@ -1,6 +1,5 @@
 import 'package:at_client/at_client.dart';
 // ignore: implementation_imports
-import 'package:at_client/src/service/sync_service.dart';
 
 class ServiceFactoryWithNoOpSyncService extends DefaultAtServiceFactory {
   @override
